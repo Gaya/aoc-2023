6dcf76cd2e02de263e3fde75cbfa9a7b5cc92b0e
Gaya Kessler <gaya@theclevernode.com>
2022-12-12 10:59:37 +0100

    day-01: solved

--- src/01-calorie-counting/list-calories.test.ts
+++ src/01-calorie-counting/list-calories.test.ts
@@ -1,4 +1,4 @@
-import listCalories from './list-calories';
+import listCalories, { parseInput } from './list-calories';
 
 const input = `1000
 2000
@@ -13,10 +13,23 @@ const input = `1000
 8000
 9000
 
-10000`;
+10000
+`;
+
+describe('parseInput', () => {
+  it('should split input in groups of items', () => {
+    expect(parseInput(input)).toStrictEqual([
+      [1000, 2000, 3000],
+      [4000],
+      [5000, 6000],
+      [7000, 8000, 9000],
+      [10000],
+    ]);
+  });
+});
 
 describe('listCalories', () => {
   it('should separate input and sum caloric contents of items', () => {
-    expect(listCalories(input)).toBe([6000, 4000, 11000, 24000, 10000]);
+    expect(listCalories(input)).toStrictEqual([6000, 4000, 11000, 24000, 10000]);
   });
 });
