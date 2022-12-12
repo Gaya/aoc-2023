7a9204703386d336ff350536f46fcceacd2bad12
Gaya Kessler <gaya@theclevernode.com>
2022-12-12 19:05:40 +0100

    day-06: solved

--- src/06-tuning-trouble/index.ts
+++ src/06-tuning-trouble/index.ts
@@ -1,11 +1,13 @@
 import { readFile } from 'fs/promises';
 
+import { findMarkerIndex } from './marker';
+
 // https://adventofcode.com/2022/day/6
 export default async function solution() {
   const input = (await readFile(`${__dirname}/input.txt`)).toString();
 
-  const part1 = 0;
-  const part2 = 0;
+  const part1 = findMarkerIndex(input);
+  const part2 = findMarkerIndex(input, 14);
 
   console.log(`Day 6 - Part 1: ${part1}`);
   console.log(`Day 6 - Part 2: ${part2}`);
