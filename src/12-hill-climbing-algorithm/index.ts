76ab25b188f1795a138e22e88dcd8eff6f62b79e
Gaya Kessler <gaya@theclevernode.com>
2022-12-14 20:20:11 +0100

    day-12: add solution calculation

--- src/12-hill-climbing-algorithm/index.ts
+++ src/12-hill-climbing-algorithm/index.ts
@@ -1,10 +1,14 @@
 import { readFile } from 'fs/promises';
 
+import { findPath, parseInput } from './path';
+
 // https://adventofcode.com/2022/day/12
 export default async function solution() {
   const input = (await readFile(`${__dirname}/input.txt`)).toString();
 
-  const part1 = 0;
+  const path = findPath(parseInput(input));
+
+  const part1 = path.length - 1;
   const part2 = 0;
 
   console.log(`Day 12 - Part 1: ${part1}`);
